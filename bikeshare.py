91f0f6eba6cf0bd610130716df5e44f608e88a24
Seth Hill <sethhill28@gmail.com>
2020-04-22 21:13:59 -0700

    Changed function description

--- bikeshare.py
+++ bikeshare.py
@@ -110,7 +110,7 @@ def load_data(city, month, day):
         print('Error occurred, could not load the file: {}'.format(e))
 
 def time_stats(df, city):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays stats on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
@@ -141,7 +141,7 @@ def time_stats(df, city):
     print('-'*40)
 
 def station_stats(df, city):
-    """Displays statistics on the most popular stations and trip."""
+    """Displays stats on the most popular stations and trip."""
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
@@ -173,7 +173,7 @@ def station_stats(df, city):
 
 
 def trip_duration_stats(df, city):
-    """Displays statistics on the total and average trip duration."""
+    """Displays stats on the total and average trip duration."""
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
@@ -195,7 +195,7 @@ def trip_duration_stats(df, city):
     print('-'*40)
 
 def user_stats(df,city):
-    """Displays statistics on bikeshare users."""
+    """Displays stats on bikeshare users."""
 
 
     start_time = time.time()
